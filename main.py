db784a96893239bef372418d6112519ce9cb18fc
wienerwarrior <sauteboy@gmail.com>
2024-07-28 14:36:13 -0700

    still trying to fix the login

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import os
 import discord
 from discord.ext import commands
 
-slurs = ['fag', 'faggot', 'nigger', 'retard', 'spic', 'chink']
+slurs = ['fag', 'faggot', 'nigger', 'retard', 'spic', 'chink', 'rape', 'raped', 'tranny', 'trany', 'homo']
 
 REPORT_CHANNEL_ID = 1266626575261106186
 
 intents = discord.Intents.default()
-intents.message_content = True  # Corrected to use message_content for intents
+intents.message_content = True  # Correct intent for reading message content
 
+# Initialize the bot
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
@@ -21,7 +22,7 @@ async def on_message(message):
         return
 
     if any(slur in message.content.lower() for slur in slurs):
-        await message.channel.send(f"{message.author.mention}, Woah dude. Dont Say that. Thats warning 1.")
+        await message.channel.send(f"{message.author.mention}, Woah dude. Don't say that. That's warning 1.")
 
         report_channel = bot.get_channel(REPORT_CHANNEL_ID)
         if report_channel:
@@ -29,5 +30,10 @@ async def on_message(message):
 
     await bot.process_commands(message)
 
+print("Getting token from environment...")
 token = os.getenv('DISCORD_BOT_TOKEN')
-bot.run(token)
+if token is None:
+    print("Error: DISCORD_BOT_TOKEN is not set.")
+else:
+    print("Token found, running bot...")
+    bot.run(token)
